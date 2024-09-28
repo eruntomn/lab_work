daed975c2957e0d4116844693affc89c342f19bd
vitalia <114501121+eruntomn@users.noreply.github.com>
2024-09-28 18:32:32 +0200

    28.09

---
+++ laba_2/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <iomanip>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+int main()
+{
+	//#4
+	double r;
+	cin >> r;
+	cout << "Площадь круга равна " << setprecision(9) << fixed << M_PI * (r * r) << endl;
+
+}
