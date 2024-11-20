bd12e0a6dcf12f407b6095e259d7decffe152692
vitalia <114501121+eruntomn@users.noreply.github.com>
2024-11-20 09:51:09 +0200

    Update main.cpp

--- laba_3/main.cpp
+++ laba_3/main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
 	cout << "Введите x и i-тый бит числа " << endl;
-	int x, i;
+	unsigned int x, i;
 	cin >> x >> i;
-	cout << (x | (1 << i)) << endl;
-
+	if ((x & (1 << i)) == 0) cout << "i-тый бит числа равен 0";
+	else cout << "i-тый бит числа равен 1";
 }
